e1c9ac945bcbbbfb37ebb2ed235d4b0fde880bc1
Aestorgs <dylan.bo@institutsolacroup.com>
2022-11-19 12:17:23 +0100

    update saison

--- src/components/saison.jsx
+++ src/components/saison.jsx
@@ -1,7 +1,31 @@
+import React from "react";
+import img from "../img/imgNotFound.png";
+import { useParams } from "react-router-dom";
+
 export const Saison = () => {
+    const {id} = useParams()
+    const [data, setData] = React.useState([]);
+    
+      React.useEffect(() => {
+        fetch(`https://api.tvmaze.com/shows/${id}/seasons`)
+          .then((res) => res.json())
+          .then((data) => setData(data))
+          .catch((err) => console.log(err));
+      }, []);
+
+
     return(
         <>
         <h1>Saison</h1>
+        <div className="list">
+        {data.map((p, index) => {
+          return (
+            <div key={index}>
+              <img src={p.image?.medium || img} />
+            </div>
+          );
+        })}
+      </div>
         </>
     )
 }
