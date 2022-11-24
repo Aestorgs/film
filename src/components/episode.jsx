94539dfa5a7d6f9eabc7bd696f7205e4dede71a0
Aestorgs <dylan.bo@institutsolacroup.com>
2022-11-24 12:29:20 +0100

    update episode

--- src/components/episode.jsx
+++ src/components/episode.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import img from "../img/imgNotFound.png";
 import { useParams } from "react-router-dom";
 
-
 export const Episodes = () => {
   const { id } = useParams();
   const [data, setData] = React.useState({});
-  console.log(id)
+  const [message, setMessage] = React.useState("");
 
   React.useEffect(() => {
     fetch(`https://api.tvmaze.com/episodes/${id}`)
@@ -15,16 +14,41 @@ export const Episodes = () => {
       .catch((err) => console.log(err));
   }, []);
 
+  const episodesSaison = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/episode/saison", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          episode: data.id,
+          vue: false,
+          saison: data.season,
+        }),
+      });
+      if (res.status === 201) {
+        setMessage({ id: data.id, message: "vue" });
+      } else {
+        res.status === 400 && setMessage("Some error occured");
+      }
+    } catch (err) {
+      console.log("ERREUR", err);
+    }
+  };
+
   return (
     <>
-    <h1>Details Episode {data.number}</h1>
-     <h2>{data.name}</h2>
-     {console.log(data)}
-        <div className="list">
-            <div>
-              <img src={data.image?.medium || img} />
-              <p>{data.summary}</p>
-            </div>
+      <h1>Details Episode {data.number}</h1>
+      <h2>{data.name}</h2>
+      {console.log(data)}
+      <div className="list">
+        <div>
+          <img src={data.image?.medium || img} />
+          <p>{data.summary}</p>
+          <button onClick={() => episodesSaison()}>vue</button>
+             {data.id === message.id && message.message}
+        </div>
       </div>
     </>
   );
